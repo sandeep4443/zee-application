e9ad864dcb58ec35297c2642cd73a82be1ae08dd
sandeep4443 <42565891+sandeep4443@users.noreply.github.com>
2021-02-03 14:58:50 +0530

    c

--- src/Pagination/Pagination.js
+++ src/Pagination/Pagination.js
@@ -6,7 +6,13 @@ import MoviesList from "../movies.json";
 const Pagination = (props) => {
   const [posts] = useState(MoviesList.slice(0, 1000));
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(10); //get current posts  const indexOfLastPost = currentPage * postsPerPage;  const indexOfFirstPost = indexOfLastPost - postsPerPage;  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const [postsPerPage] = useState(10); 
+  
+  //get current posts  
+  
+  const indexOfLastPost = currentPage * postsPerPage;  
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;  
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
   const paginate = (pageNumber) => {
     setCurrentPage(pageNumber);
