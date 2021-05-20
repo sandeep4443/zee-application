f58f4c5057a5d726c95b96fa2c793d57227522fd
sandeep4443 <42565891+sandeep4443@users.noreply.github.com>
2021-05-20 10:31:42 +0530

    filters

--- src/AddRemove/AddElements.js
+++ src/AddRemove/AddElements.js
@@ -1,43 +1,45 @@
 import React, { useState } from 'react'
 
 const AddElements = () => {
-    const [inputValues, setValues] = useState([{ firstName: '', mail: '' }]);
+    const [inputValues, setValues] = useState([
+        {
+            firstName: '',
+            mail: '',
+            error: false,
+            errorMessage: ''
+        }
+    ]);
 
     const handleInputChange = (e, index) => {
         const { name, value } = e.target;
         const list = [...inputValues];
         list[index][name] = value;
-        setValues(list);
         for (let i = 0; i < inputValues.length; i++) {
             for (let j = i + 1; j < inputValues.length; j++) {
                 if (inputValues.length > 1) {
                     if ((list[i].firstName !== '' && list[j].firstName !== '') && (list[i].mail !== '' && list[j].mail !== '')) {
                         if ((list[i].firstName === list[j].firstName) && (list[i].mail === list[j].mail)) {
-                            alert(`both First Name && Mail cannot be same`)
+                            list[i].error = true;
+                            list[i].errorMessage = "Duplicate Entries";
+                            list[j].error = true;
+                            list[j].errorMessage = "Duplicate Entries";
+                        } else {
+                            list[i].error = false;
+                            list[i].errorMessage = '';
+                            list[j].error = false;
+                            list[j].errorMessage = '';
                         }
                     }
                 }
             }
         }
+        setValues(list);
     };
 
     const removeElement = index => {
         const list = [...inputValues];
         list.splice(index, 1);
         setValues(list);
-        /*
-           if (inputValues.length === 1) {
-                var confirmDelete = confirm("Are you sure you want to delete ? Deleting now will loose all the information. You still want to continue ?");
-                if (confirmDelete) {
-                    const list = [...inputValues];
-                    list.splice(index, 1);
-                    setValues(list);
-                }
-            } else {
-                return null;
-            }
-        **/
-
     };
 
     const addElement = () => {
@@ -47,43 +49,49 @@ const AddElements = () => {
         <div style={{ margin: 30 + 'px' }}>
             {inputValues.map((val, i) => {
                 return (
-                    <div className="row">
-                        <div className="col-md-3">
-                            <label>Enter Name:</label>
-                            <input
-                                name="firstName"
-                                placeholder="Enter First Name"
-                                value={val.firstName}
-                                onChange={e => handleInputChange(e, i)}
-                            />
-                        </div>
-                        <div className="col-md-3">
-                            <label>Enter Mail:</label>
-                            <input
-                                name="mail"
-                                placeholder="Enter mail id"
-                                value={val.mail}
-                                onChange={e => handleInputChange(e, i)}
-                            />
-                        </div>
-                        <div className="col-md-3" style={{ marginTop: 30 + 'px' }}>
-                            {inputValues.length != 1 &&
+                    <div>
+                        <div className="row">
+                            <div className="col-md-3">
+                                <label>Enter Name:</label>
+                                <input
+                                    name="firstName"
+                                    placeholder="Enter First Name"
+                                    value={val.firstName}
+                                    onChange={e => handleInputChange(e, i)}
+                                />
+                            </div>
+                            <div className="col-md-3">
+                                <label>Enter Mail:</label>
+                                <input
+                                    name="mail"
+                                    placeholder="Enter mail id"
+                                    value={val.mail}
+                                    onChange={e => handleInputChange(e, i)}
+                                />
+                            </div>
+                            <div className="col-md-3" style={{ marginTop: 30 + 'px' }}>
+                                {inputValues.length != 1 &&
+                                    <input
+                                        type="button"
+                                        className="removeButton"
+                                        value="Remove"
+                                        onClick={() => removeElement(i)}
+                                    />
+                                }
+                            </div>
+                            <div className="col-md-3" style={{ marginTop: 30 + 'px' }}>
                                 <input
                                     type="button"
-                                    className="removeButton"
-                                    value="Remove"
-                                    onClick={() => removeElement(i)}
+                                    className="addButton"
+                                    value="Add"
+                                    onClick={addElement}
                                 />
-                            }
+                            </div>
                         </div>
-                        <div className="col-md-3" style={{ marginTop: 30 + 'px' }}>
-                            <input
-                                type="button"
-                                className="addButton"
-                                value="Add"
-                                onClick={addElement}
-                            />
+                        <div>
+                            {val.error && <strong style={{ color: 'red' }}>{val.errorMessage}</strong>}
                         </div>
+
                     </div>
                 )
             })
@@ -92,4 +100,4 @@ const AddElements = () => {
     )
 }
 
-export default AddElements
+export default AddElements;
